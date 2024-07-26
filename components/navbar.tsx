807b361bdd94264584c7ef7fe13856915b252107
Your Name <you@example.com>
2024-07-26 17:23:08 +0545

    CoreX Convert

---
+++ components/navbar.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { BsGithub } from "react-icons/bs";
+import { ModeToggle } from "./mode-toggle";
+
+export default function Navbar({}): any {
+  return (
+    <nav className="fixed z-50 flex items-center justify-between w-full h-24 px-4 py-10 backdrop-blur-md bg-background bg-opacity-30 md:px-8 lg:px-12 xl:px-16 2xl:px-24">
+      <Link href="/">
+        <Image
+          alt="logo"
+          className="mb-2 cursor-pointer w-35 dark:invert"
+          src="https://iili.io/dzAoIqb.png"
+          height={100}
+          width={170}
+        />
+      </Link>
+      <div className="items-center hidden gap-2 md:flex">
+        <ModeToggle />
+       
+        
+      </div>
+      <div className="block p-3 md:hidden ">
+        <ModeToggle />
+      </div>
+    </nav>
+  );
+}
