532aa922bfe0f69682ae2e0907444fb48d9d260c
Sagar Bhusal <127485363+sagar479@users.noreply.github.com>
2024-07-26 17:48:37 +0545

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
     
       <div className="space-y-6">
         <h1 className="text-3xl font-medium text-center md:text-5xl">
-          Free Unlimited File Converter with CoreX Convert
+          Free Unlimited File Converter with Free Convert
         </h1>
         <p className="text-center text-muted-foreground text-md md:text-lg md:px-24 xl:px-44 2xl:px-52">
-          Introducing CoreX Convert – your go-to online tool for unlimited and free
+          Introducing Free Convert – your go-to online tool for unlimited and free
           multimedia conversion. Easily convert images, audio, and videos
           without any restrictions. Start converting now and streamline your
-          content effortlessly with CoreX Convert!
+          content effortlessly with Free Convert!
         </p>
       </div>
       <div className="space-y-6">      <center>  <p>&copy; 2024 Sagar Bhusal</p> </center>
